1fe3692febb9c3016dd19d3aeb8557c7dc3f8afe
durzal1 <67489054+durzal1@users.noreply.github.com>
2021-09-12 11:10:35 -0400

    move ordering

---
+++ cmake-build-release/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/TranspositionTable.cpp.obj"
+  "CMakeFiles/Chess.dir/ai.cpp.obj"
+  "CMakeFiles/Chess.dir/board.cpp.obj"
+  "CMakeFiles/Chess.dir/evaluate.cpp.obj"
+  "CMakeFiles/Chess.dir/getPossibleMoves.cpp.obj"
+  "CMakeFiles/Chess.dir/main.cpp.obj"
+  "CMakeFiles/Chess.dir/piece.cpp.obj"
+  "CMakeFiles/Chess.dir/uci.cpp.obj"
+  "Chess.exe"
+  "Chess.exe.manifest"
+  "Chess.pdb"
+  "libChess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
